e45f485dd40374ec1f26338e26431655c2e83c19
yudar1024 <rojer.chen@foxmail.com>
2019-09-10 16:08:00 +0800

    update

--- installation/storage/gluster-heketi-standalone/run-on-hektiserver-node.sh
+++ installation/storage/gluster-heketi-standalone/run-on-hektiserver-node.sh
@@ -1,12 +1,16 @@
+#安装
 yum install heketi heketi-client -y
 modprobe dm_thin_pool
-cp heketi.json /etc/heketi/
-cp topology.json /etc/heketi/
+#heketi 免密
 ssh-keygen -t rsa -q -f /var/lib/heketi/id_rsa -N ''
 chown heketi:heketi /var/lib/heketi/id_rsa*
 ssh-copy-id -i /var/lib/heketi/id_rsa root@node1
 ssh-copy-id -i /var/lib/heketi/id_rsa root@node2
 ssh-copy-id -i /var/lib/heketi/id_rsa root@node3
+# heketi 配置
+cp heketi.json /etc/heketi/
+cp topology.json /etc/heketi/
+#启动heketi
 systemctl start heketi
 systemctl enable heketi
 export HEKETI_CLI_SERVER=http://192.168.10.50:8888
