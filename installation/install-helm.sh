88536bf568b72abd83f472f2750b2baf45cf9f53
yudar1024 <rojer.chen@foxmail.com>
2019-10-16 15:12:39 +0800

    adapt k8s 1.16

--- installation/install-helm.sh
+++ installation/install-helm.sh
@@ -1,9 +1,16 @@
-wget https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz
+if [ ! -f "helm-v2.14.3-linux-amd64.tar.gz" ]; then
+wget -c -t 0 https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz
+fi
 tar -zxf helm-v2.14.3-linux-amd64.tar.gz
 chmod +x linux-amd64/helm
 mv linux-amd64/helm /usr/local/bin
 kubectl create serviceaccount --namespace=kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.14.3 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts --service-account=tiller
-
+version=`kubeadm version -o short`
+# note 正则不能使用引号，变量可以使用引号
+if [[ "$version" =~ ^1.16 ]]; then
+helm init -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.14.3 --stable-repo-url http://mirror.azure.cn/kubernetes/charts/ --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
+else
+helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.14.3 --stable-repo-url http://mirror.azure.cn/kubernetes/charts/ --service-account=tiller
+fi
 
--- installation/network-deploy.sh
+++ installation/network-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install wget -y
-wget https://docs.projectcalico.org/v3.9/manifests/calico.yaml
+wget -c -t 0 https://docs.projectcalico.org/v3.9/manifests/calico.yaml
 # 删除 CALICO_IPV4POOL_CIDR的下一行
 sed -i '/CALICO_IPV4POOL_CIDR/{n;d}' calico.yaml
 # 需与kubeadm-init.yaml 中的 podsubnet 一致
